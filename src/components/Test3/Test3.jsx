e8292974de0ec28466c63427b0800ec5f1688f47
chohanac <anaschohanto@gmail.com>
2025-04-06 20:35:47 -0400

    changed header style

--- src/components/Test3/Test3.jsx
+++ src/components/Test3/Test3.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './Test3.scss';
 
 const Test3 = () => {
-    // Create an array of numbers from 1 to 9
     const numbers = Array.from({ length: 9 }, (_, i) => i + 1);
 
     return (
